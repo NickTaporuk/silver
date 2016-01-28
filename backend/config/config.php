719d726fafc4b8052dbe16abf7359bfdf9e1566d
NickTaporuk <nictaporuk@yandex.ru>
2016-01-28 13:25:52 +0200

    add init silver job php in html, add new folder to framework

---
+++ backend/config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+
+return [
+    'db' => [
+        'local' => [
+            'db_name' => 'silver',
+            'db_user' => 'root',
+            'db_host' => 'localhost'
+        ]
+    ]
+];
